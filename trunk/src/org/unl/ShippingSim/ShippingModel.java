3d4954bb001d9e21b0521735a785ea52a21e97d4
djw8605@gmail.com <djw8605@gmail.com@e970c010-ba24-3505-ad32-845dcd160654>
2011-10-24 01:51:41 +0000

    Adding the ShippingModel, the base class of a shipping model.

---
+++ trunk/src/org/unl/ShippingSim/ShippingModel.java
@@ -0,0 +1,48 @@
+package org.unl.ShippingSim;
+
+import uchicago.src.sim.engine.SimpleModel;
+
+public class ShippingModel extends SimpleModel {
+
+	protected int numBoats;
+	
+	public ShippingModel() {
+		this.params = new String [] {"NumberBoats"};
+		this.name = "Shipping Simulation";
+		
+	}
+	
+	public void setup() {
+		super.setup();
+		
+		// Initialize the boat & Harbor factories
+		
+	}
+	
+	public void buildModel() {
+		// Add new boats & harbors to the agentlist
+		
+	}
+
+	
+	public void step() {
+		int size = agentList.size();
+		for (int i = 0; i < size; i++) {
+			Player p = (Player) agentList.get(i);
+			p.play();
+		}
+	}
+	
+	
+	/* Parameter functions */
+	
+	
+	public void setNumberBoats(int val) {
+		this.numBoats = val;
+	}
+	public int getNumberBoats() {
+		return this.numBoats;
+	}
+	
+
+}
