976789b86438f27c675e1b4e3662b599ac81b7ac
djw8605@gmail.com <djw8605@gmail.com@e970c010-ba24-3505-ad32-845dcd160654>
2011-10-25 16:27:15 +0000

    Adding a create items function.

--- src/org/unl/ShippingSim/HarborFactory.java
+++ src/org/unl/ShippingSim/HarborFactory.java
@@ -1,7 +1,14 @@
 package org.unl.ShippingSim;
 
+import java.util.ArrayList;
+
+import uchicago.src.sim.util.Random;
+
 public class HarborFactory {
 
+	
+	protected int numItems = 4;
+	
 	// Initialize stuff
 	public HarborFactory() {
 		
@@ -10,12 +17,29 @@ public class HarborFactory {
 	
 	
 	public HarborAgent createHarbor(int x, int y, OceanSpace space) {
-		HarborAgent harbor = new HarborAgent(x, y, space);
+		
+		
+		ArrayList<SellableItem> items = createItems(numItems);
+		HarborAgent harbor = new HarborAgent(x, y, space, items);
 		
 		// Do harbor initializaation
 		
 		
 		return harbor;
+	}
+	
+	
+	public ArrayList<SellableItem> createItems(int numItems) {
+		ArrayList<SellableItem> items = new ArrayList<SellableItem>();
+		for (int i = 0; i < numItems; i++) {
+			// Probably should do some distribution of item values and whatnot
+			double baseline = Random.normal.nextDouble(50.0, 100.0);
+			double initial_inv = Random.normal.nextDouble(100.0, 200.0);
+			SellableItem item = new SellableItem((float)baseline, (float)initial_inv);
+			items.add(item);
+			
+		}
+		return items;
 	}
 	
 }
