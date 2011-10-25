ec9e58f23033e3f254167437caa19c1f7885b604
djw8605@gmail.com <djw8605@gmail.com@e970c010-ba24-3505-ad32-845dcd160654>
2011-10-25 01:02:12 +0000

    Adding harbor stuff.

--- trunk/src/org/unl/ShippingSim/SellableItem.java
+++ trunk/src/org/unl/ShippingSim/SellableItem.java
@@ -2,6 +2,20 @@ package org.unl.ShippingSim;
 
 public class SellableItem {
 
+	// Selling stuff
+	protected double sellValue = 0.0;
+	protected double buyValue = 0.0;
+	
+	protected double inventory = 0.0;
+	protected double baseline = 0.0;
+	
+	public SellableItem(float baseline, float starting_inventory) {
+		this.baseline = baseline;
+		this.inventory = starting_inventory;
+	}
+	
+	
+	
 	
 	
 }
