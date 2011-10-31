e62fe2314f997e8e2dcc4bedae975d87b4f7b75e
djw8605 <djw8605@e970c010-ba24-3505-ad32-845dcd160654>
2011-10-31 20:27:35 +0000

    Adding initial logic for picking a next harbor.

--- src/org/unl/ShippingSim/BoatFactory.java
+++ src/org/unl/ShippingSim/BoatFactory.java
@@ -12,11 +12,12 @@ public class BoatFactory {
 		
 	}
 	
-	public BoatAgent CreateBoat(int x, int y, OceanSpace space) {
+	public BoatAgent CreateBoat(int x, int y, OceanSpace space, HarborAgent init_harbor) {
 		BoatAgent b = new BoatAgent(x, y, space);
 		// Do the initialization stuff
 		float boat_size = Random.uniform.nextFloatFromTo(100, 300);
 		b.setSize(boat_size);
+		b.SetHarbor(init_harbor);
 		
 		return b;
 		
